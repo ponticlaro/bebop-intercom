1bd4187e69e3ea5ba70c40443fdd90c9446b23d1
Cristiano Baptista <cristianobaptista@designdesu.com>
2015-09-23 21:14:29 +0200

    fixed double Intercom widget when checking plugin details within an iframe

--- assets/js/bebop-intercom.js
+++ assets/js/bebop-intercom.js
@@ -1,47 +1,54 @@
 ;(function(window, document, undefined, $){
 
-    // Load Intercom Javascript library
-    (function(){
-    	var w=window;
-    	var ic=w.Intercom;
-    	if(typeof ic==="function"){
-    		ic('reattach_activator');
-    		ic('update', window.intercomSettings);
-    	}
+    // Check if we're inside an iframe
+    var isInIframe = window.frameElement && window.frameElement.nodeName == "IFRAME";
 
-    	else{
-    		var d=document;
-    		var i=function(){
-    			i.c(arguments);
-    		};
-    		i.q=[];
-    		i.c=function(args){
-    			i.q.push(args);
-    		};
-    		w.Intercom=i;
+    // Load intercom only if not inside an iframe
+    if (!isInIframe) {
 
-    		var l = function(){
-    			var s=d.createElement('script');
-    			s.type='text/javascript';
-    			s.async=true;
-    			s.src='https://widget.intercom.io/widget/jki7e6dv';
-    			var x=d.getElementsByTagName('script')[0];
-    			x.parentNode.insertBefore(s, x);
-    		};
+        // Load Intercom Javascript library
+        (function(){
+        	var w=window;
+        	var ic=w.Intercom;
+        	if(typeof ic==="function"){
+        		ic('reattach_activator');
+        		ic('update', window.intercomSettings);
+        	}
 
-    		if(w.attachEvent){
-    			w.attachEvent('onload', l);
-    		}
+        	else{
+        		var d=document;
+        		var i=function(){
+        			i.c(arguments);
+        		};
+        		i.q=[];
+        		i.c=function(args){
+        			i.q.push(args);
+        		};
+        		w.Intercom=i;
 
-    		else{
-    			w.addEventListener('load', l, false);
-    		}
-    	}
-    })();
-    
-    // When DOM is ready...
-    $(function(){
-        window.Intercom('boot', window.intercomSettings);
-    });
+        		var l = function(){
+        			var s=d.createElement('script');
+        			s.type='text/javascript';
+        			s.async=true;
+        			s.src='https://widget.intercom.io/widget/jki7e6dv';
+        			var x=d.getElementsByTagName('script')[0];
+        			x.parentNode.insertBefore(s, x);
+        		};
+
+        		if(w.attachEvent){
+        			w.attachEvent('onload', l);
+        		}
+
+        		else{
+        			w.addEventListener('load', l, false);
+        		}
+        	}
+        })();
+        
+        // When DOM is ready...
+        $(function(){
+            window.Intercom('boot', window.intercomSettings);
+        });
+    }
 
 })(window, document, undefined, jQuery || $);
